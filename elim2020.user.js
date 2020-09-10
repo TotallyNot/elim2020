3d0d465430819c3cc3198a0cf58e6e5da19f9c5b
TotallyNot <44345987+TotallyNot@users.noreply.github.com>
2020-09-10 23:36:28 +0200

    now functional

--- elim2020.user.js
+++ elim2020.user.js
@@ -1,25 +1,23 @@
 // ==UserScript==
 // @name         Elimination filter
-// @namespace    site.elim2019
-// @version      0.0.2
+// @namespace    me.elimination
+// @version      0.1.0
 // @updateURL    https://raw.githubusercontent.com/TotallyNot/elim2020/master/elim2020.user.js
 // @description  Filter the elimination team lists using user defined conditions.
-// @author       Pyrit [2111649]
-// @match        https://www.torn.com/*
-// @match        https://elim2019.site/index*
-// @match        http://localhost:8080/index*
+// @author       Pyrit[2111649]
+// @match        https://www.torn.com/competition.php*
+// @match        https://elimination.me/index*
 // @grant        GM.xmlHttpRequest
 // @grant        GM.setValue
 // @grant        GM.getValue
 // @grant        GM.info
-// @connect      elim2019.site
-// @connect      localhost
+// @connect      elimination.me
 // @run-at       document-end
 // ==/UserScript==
 
 // {{{ convenience functions
 
-// GM.setValue("state", "{}");
+//GM.setValue("state", "{}");
 
 function gmFetch(url, config) {
     return new Promise((resolve) => {
@@ -27,7 +25,7 @@ function gmFetch(url, config) {
             url,
             method: config?.method,
             headers: config?.headers,
-            body: config?.data,
+            data: config?.data,
             onload: (response) =>
                 resolve(
                     new Response(response.response, {
@@ -112,10 +110,7 @@ listeners.push(storageListener);
 
 // {{{ website
 
-if (
-    location.hostname === "elim2019.site" ||
-    location.hostname === "localhost"
-) {
+if (location.hostname === "elimination.me") {
     const dataIn = document.querySelector("#userscript-out");
     const observer = new MutationObserver(() => {
         const token = JSON.parse(dataIn.innerText);
@@ -130,13 +125,13 @@ if (
     const tokenListener = new MemoListener(
         ["hydrated", "token", "valid"],
         ({ hydrated, token, valid }) => {
-            if (!hydrated || valid === undefined) return;
+            if (!hydrated || (token && valid === undefined)) return;
 
             const dataOut = document.querySelector("#userscript-in");
 
             dataOut.innerText = JSON.stringify({
                 version: GM.info.script.version,
-                linked: token && valid,
+                linked: token !== undefined && valid === true,
             });
         }
     );
@@ -220,8 +215,8 @@ if (location.hostname === "www.torn.com") {
     document.querySelector("head").appendChild(styles);
 
     const infoWidget = new MemoListener(
-        ["token", "toggled", "valid", "filters"],
-        ({ token, toggled, valid, filters }) => {
+        ["token", "toggled", "valid", "filters", "error"],
+        ({ token, toggled, valid, filters, error }) => {
             let mountPoint = document.querySelector("#elim-mount-point");
             if (mountPoint === null) {
                 mountPoint = document.createElement("div");
@@ -232,7 +227,9 @@ if (location.hostname === "www.torn.com") {
             }
 
             let content = undefined;
-            if (!token) {
+            if (error) {
+                content = `<span class="elim-error">Error: ${error}</span>`;
+            } else if (!token) {
                 content = `<span class="elim-error">Please link the script to your account by visiting the settings.</span>`;
             } else if (valid === undefined) {
                 content = "";
@@ -258,7 +255,7 @@ if (location.hostname === "www.torn.com") {
     </label>`
             : ""
     }
-    <a href="https://elim2019.site/index" target="_blank" class="elim-button">
+    <a href="https://elimination.me/index" target="_blank" class="elim-button">
         <svg style="width:15px;height:15px" viewBox="0 0 24 24">
             <path fill="currentColor" d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
         </svg>
@@ -273,28 +270,85 @@ if (location.hostname === "www.torn.com") {
     );
     listeners.push(infoWidget);
 
-    const playerListener = new MemoListener(
-        ["players", "toggled", "valid", "filters", "filteredIDs"],
-        ({ players, toggled, valid, filters, filteredIDs }) => {
-            if (!players) return;
+    const competitionWrap = document.querySelector("#competition-wrap");
 
-            if (!toggled || !valid || !filters) {
-                player.forEach((player) => {
-                    player.style.display = "block";
-                });
-            }
-            if (toggled) {
-                if (!filteredIDs) {
-                    player.forEach((player) => {
-                        player.style.display = "none";
-                    });
-                } else {
-                    // TODO: filter ids and make visible...
+    const observer = new MutationObserver((mutations) => {
+        if (
+            mutations.some((mutation) => mutation.addedNodes.length > 0) &&
+            location.href.indexOf("&team=") !== -1
+        ) {
+            const users = [
+                ...competitionWrap.querySelectorAll(".competition-list > li"),
+            ];
+            reducer({ users });
+        }
+    });
+    observer.observe(competitionWrap, { subtree: true, childList: true });
+
+    const userListener = new MemoListener(
+        ["users", "token", "valid"],
+        ({ users, token, valid }) => {
+            if (!token || !valid) return;
+            const userIDs = users.map((user) =>
+                parseInt(
+                    user.querySelector(".name .user").href.match(/[0-9]+/)[0]
+                )
+            );
+
+            gmFetch(
+                `https://elimination.me/api/elimination/filter?ids=${userIDs.join(
+                    ","
+                )}`,
+                {
+                    headers: {
+                        authorization: `Bearer ${token}`,
+                    },
                 }
+            ).then((response) => {
+                if (response.ok) {
+                    return response
+                        .json()
+                        .then((filteredIDs) =>
+                            reducer({ filteredIDs, error: null })
+                        );
+                } else {
+                    return response
+                        .json()
+                        .then((error) => reducer({ error: error.reason }));
+                }
+            });
+        }
+    );
+    listeners.push(userListener);
+
+    const filterListener = new MemoListener(
+        ["toggled", "users", "filteredIDs"],
+        ({ toggled, users, filteredIDs }) => {
+            if (location.href.indexOf("terror-bytes") !== -1 || !users) return;
+
+            if (!toggled) {
+                users.forEach((user) => user.style.removeProperty("display"));
+                return;
+            }
+
+            users.forEach((user) => (user.style.display = "none"));
+
+            if (filteredIDs) {
+                users
+                    .filter((user) =>
+                        filteredIDs.includes(
+                            parseInt(
+                                user
+                                    .querySelector(".name .user")
+                                    .href.match(/[0-9]+/)[0]
+                            )
+                        )
+                    )
+                    .forEach((user) => user.style.removeProperty("display"));
             }
         }
     );
-    listeners.push(playerListener);
+    listeners.push(filterListener);
 }
 
 // }}}
@@ -305,7 +359,7 @@ const tokenListener = new MemoListener(
     ["token"],
     ({ token }) =>
         token &&
-        gmFetch("http://localhost:8080/api/elimination/status", {
+        gmFetch("https://elimination.me/api/elimination/status", {
             headers: {
                 authorization: `Bearer ${state.token}`,
             },
